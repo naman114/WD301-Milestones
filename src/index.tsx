bf1a68928e97643586bacfb9bdfdbab173d0af40
Naman Gogia <namangogia2001@gmail.com>
2022-04-22 04:17:54 +0530

    Add production env

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,17 @@ import { BrowserTracing } from "@sentry/tracing";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-Sentry.init({
-  dsn: "https://72fc289ee3da4f258dde52c4916b2131@o1214350.ingest.sentry.io/6354820",
-  integrations: [new BrowserTracing()],
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: "https://72fc289ee3da4f258dde52c4916b2131@o1214350.ingest.sentry.io/6354820",
+    integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
